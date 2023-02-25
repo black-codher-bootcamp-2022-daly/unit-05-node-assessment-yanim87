595e9f663383f2d16cf1ff4f9643886cf32528e5
Yetunde Animashawun <yanim87@gmail.com>
2023-02-25 15:51:46 +0000

    style and html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+// require('dotenv').config();
+// const fs = require('fs');
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const port = 8080;
+// const bodyParser = require('body-parser');
+// const { v4: uuidv4 } = require('uuid');
+// const todoFilePath = process.env.BASE_JSON_PATH;
+// const err = console.error();
+
+
+// //Read todos from todos.json into variable
+// let todos = require(__dirname + todoFilePath);
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.raw());
+// app.use(bodyParser.json());
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", (_, res) => {
+  
+//   res.sendFile("./public/index.html", { root: __dirname });
+//   console.log(err);
+//   // res.status(200).end();
+// });
 require('dotenv').config();
 const fs = require('fs');
 const express = require('express');
@@ -6,30 +34,32 @@ const path = require('path');
 const port = 8080;
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
+const { doesNotMatch } = require('assert');
 const todoFilePath = process.env.BASE_JSON_PATH;
 
-//Read todos from todos.json into variable
-let todos = require(__dirname + todoFilePath);
+
+//Read todos from todos.json into variable todos
+const todos = require(__dirname + todoFilePath);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.raw());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/content", express.static(path.join(__dirname, "public")));
+app.get("/", (_req, res) => {
+  res.sendFile("./public/index.html", { root: __dirname }, (err) => {
+    console.log(err);
+  });
 
-app.get("/", (_, res) => {
-  /*
-  res.sendFile("./public/index.html", { root: __dirname });
-  */
-  res.status(501).end();
 });
 
 app.get('/todos', (_, res) => {
-  /*
+  
   res.header("Content-Type","application/json");
-  res.sendFile(todoFilePath, { root: __dirname });
-  */
+   res.sendFile(todoFilePath, { root: __dirname });
+  console.log(todos);
+
   res.status(501).end();
 });
 
